705a95296e50990a896cf0a33221cc11a2f17ee8
Rick Martinez <rick@typeischeap.com>
2021-05-17 13:32:21 -0500

    Added SupplyChainSandbox trivia blurb and link.

--- src/pages/games.js
+++ src/pages/games.js
@@ -10,6 +10,7 @@ import pic10 from '../assets/images/helena-yankovska-sJOy9pveFbU-unsplash.jpg'
 import pic11 from '../assets/images/quad-blocks-quiz.jpg'
 import pic12 from '../assets/images/jeopardy.jpg'
 import pic13 from '../assets/images/quad-block-logo-1600.png'
+import pic14 from '../assets/images/sandbox-trivia.jpg'
 
 
 const Games = (props) => (
@@ -61,6 +62,22 @@ const Games = (props) => (
                 </section>
                 <section>
                     <Link to="/generic" className="image">
+                        <img src={pic14} alt="" />
+                    </Link>
+                    <div className="content">
+                        <div className="inner">
+                            <header className="major">
+                                <h3>SupplyChainSandbox Trivia</h3>
+                            </header>
+                            <p>Test your Supply Chain security logistics, policy and technology knowledge. Are you responsible for supply chain in your current role? Do you have what it takes to pass the test?</p>
+                            <ul className="actions">
+                                <li><Link to="https://www.quiz-maker.com/Q7YAGXTLS" className="button">PLAY NOW</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <Link to="/generic" className="image">
                         <img src={pic12} alt="" />
                     </Link>
                     <div className="content">
@@ -71,6 +88,22 @@ const Games = (props) => (
                             <p>Protecting your organization’s information in a digitally-connected world means understanding not only your organization’s immediate supply chain, but also the security of the extended ecosystem and all entities involved. The SupplyChainSandbox Jeopardy group is educating the public about supply chain risk management through an interactive fun and educational experience.</p>
                             <ul className="actions">
                                 <li><Link to="https://github.com/SupplyChainSandbox/supplychainjeopardy" className="button">Learn more</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <Link to="/generic" className="image">
+                        <img src={pic10} alt="" />
+                    </Link>
+                    <div className="content">
+                        <div className="inner">
+                            <header className="major">
+                                <h3>SBOM Mixology</h3>
+                            </header>
+                            <p>The SupplychainSanbox Sipping Bill of Materials group's intent is to provide a Bill of Materials in a fun and educational way by applying the concept to mixology!</p>
+                            <ul className="actions">
+                                <li><Link to="/sbom" className="button">Learn more</Link></li>
                             </ul>
                         </div>
                     </div>
@@ -91,22 +124,6 @@ const Games = (props) => (
                               <li><a href="https://beergameapp.com/">A.T. Kearney (iPhone)</a></li>
                               <li><a href="https://beergame.opexanalytics.com/#/">Opex Analytics</a></li>
                             </ul> 
-                        </div>
-                    </div>
-                </section>
-                <section>
-                    <Link to="/generic" className="image">
-                        <img src={pic10} alt="" />
-                    </Link>
-                    <div className="content">
-                        <div className="inner">
-                            <header className="major">
-                                <h3>SBOM Mixology</h3>
-                            </header>
-                            <p>The SupplychainSanbox Sipping Bill of Materials group's intent is to provide a Bill of Materials in a fun and educational way by applying the concept to mixology!</p>
-                            <ul className="actions">
-                                <li><Link to="/sbom" className="button">Learn more</Link></li>
-                            </ul>
                         </div>
                     </div>
                 </section>
