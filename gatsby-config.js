3789dffd43901c08b54d1d514f51f1437e7804a7
TypeIsCheap <rick@typeischeap.com>
2021-05-16 21:32:34 -0500

    Update gtags id

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,12 +25,12 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "GTM-WKLXCHN" // Google Analytics / GA
+          "G-MG0RK6JFR2" // Google Analytics / GA
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
+          optimize_id: "GTM-WKLXCHN",
           anonymize_ip: true,
           cookie_expires: 0,
         },
