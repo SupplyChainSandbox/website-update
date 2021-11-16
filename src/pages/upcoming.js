af91c8a98ddfd36d1e22562cb7b897bf1ec09f24
Rick Martinez <rick@typeischeap.com>
2021-11-16 13:50:04 -0600

    Updates for RSAC 2022

--- src/pages/upcoming.js
+++ src/pages/upcoming.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import BannerUpcoming from '../components/BannerUpcoming'
 
-import pic08 from '../assets/images/rsac-sandbox-banner.jpg'
+import pic08 from '../assets/images/rsac-sandbox-banner-2022.jpg'
 
 const Upcoming = (props) => (
     <Layout>
@@ -18,7 +18,7 @@ const Upcoming = (props) => (
         <div id="main">
             <section id="rsac2021" className="spotlights">
                 <section>
-                    <Link to="https://www.rsaconference.com/usa?utm_source=sbox-supchainsbox&utm_medium=referral&utm_campaign=standard-textad-us2021" className="image">
+                    <Link to="https://www.rsaconference.com/usa?utm_source=sb-supchain&utm_medium=referral&utm_campaign=textad-us2022" className="image">
                         <img src={pic08} alt="" />
                     </Link>
                     <div className="content">
@@ -31,9 +31,7 @@ const Upcoming = (props) => (
                             </p>
                             <p>Tuesday, May 18 | 9:20 AM – 3:40 PM PT</p>
                             <p>
-                            Supply chain management is a critical factor of business success. Explore the impacts of supply chain issues and learn concepts and approaches to manage cyber supply chains more
-                            effectively. <Link to="https://www.rsaconference.com/usa?utm_source=sbox-supchainsbox&utm_medium=referral&utm_campaign=standard-textad-us2021">Register today</Link> for
-                            RSAC 2021 All Access Pass.
+Supply chain management is a critical factor of business success. Explore the impacts of supply chain issues and learn concepts and approaches to manage cyber supply chains more effectively. Join us in the Supply Chain Village Sandbox from Tuesday, February 8 through Thursday, February 10. Plus, <Link to="https://www.rsaconference.com/usa?utm_source=sb-supchain&utm_medium=referral&utm_campaign=textad-us2022">register today</Link> using code 32USBSCVFD to save $150 off your RSAC 2022 Full Conference Pass. Open to Full Conference and Expo Plus passholders only.
                             </p> 
                             <ul className="actions">
                                 <li><Link to="https://www.rsaconference.com/usa/agenda/full-agenda#q=sbx8&sort=%40eventstart%20ascending" className="button">see the agenda</Link></li>
